621d7d8d95026e83c08b92e4062b2235625b7ff7
alswjd073 <91234288+alswjd073@users.noreply.github.com>
2021-09-23 11:38:37 +0900

    Update BJ2577.java

--- BJ2577.java
+++ BJ2577.java
@@ -16,17 +16,18 @@ public class BJ2577 {
 		int[] arr = new int[n];
 		int[] arr2 = new int[10];
 		
-		//D¸¦ stringÀ¸·Î º¯È¯ÇØ¼­ length ±¸ÇØ
+		//Dë¥¼ stringìœ¼ë¡œ ë³€í™˜í•´ì„œ length êµ¬í•¨
 		//string d = Integer.toString(D);
 		//int n = d.length();
-		//D = 233567 --> n = 6 10^n
+		//ì˜ˆ) D = 233567 --> n = 6 10^n
 		//arr[0]=(234567/10^4)-(234567/10^6)*10=2
 		//2=2 --> arr2[2]=1
 		//arr[1]=3 arr2[3]=1
 		//arr[2]=3 --> arr2[3]=2
 		//arr[6]=7 --> arr2[7]=1
-		//23, 2¸¦ »©¾ßµÅ 
+		//23, 2ë¥¼ ë¹¼ì•¼í•¨
 		//arr[0]=2, k=0
+		
 		for(int i=0; i<n; i++) {
 			int a = (int) (Math.pow(10, (n-i-1)));
 			int b = (int) (Math.pow(10,(n-i)));

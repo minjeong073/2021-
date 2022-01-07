af9496b30755b226cac64206b050656529fb32d6
김민정 <91234288+minjeong073@users.noreply.github.com>
2022-01-07 16:47:05 +0900

    BJ 15596 N Number Sum

--- Main.java
+++ Main.java
@@ -1,35 +1,12 @@
 
 public class Main {
-	
-	//d(n) 함수
-	public static int d(int num) {
-		int sum = num;
-		while (num>0) {
-			sum += num%10;
-			num = num/10;
+
+	long sum(int[] a) {
+		int sum = 0;
+		for (int i=0; i<a.length; i++) {
+			sum += a[i];
 		}
 		return sum;
-	}
-	
-	public static void main(String[] args) {
-		
-		boolean[] check = new boolean[10001];
-		
-		//생성자 있는 숫자 true로 저장
-		for (int i=1; i<10001; i++) {
-			int n = d(i);
-			
-			if (n<10001) {
-				check[n] = true;
-			}
-		}
-		
-		//false인 숫자(=셀프넘버)만 출력
-		for (int i=1; i<10001; i++) {
-			if (!check[i]) {
-				System.out.println(i);
-			}
-		}
 	}
 
 }
